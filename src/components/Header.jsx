fcc7662fc82fdd17d2b73ad60f152be6359f3a41
Martyna Kiljan <kmartynak@icloud.com>
2024-10-28 21:55:19 +0100

    added basic stuff

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<div className='header__inner'>
+				<div className='header__image'></div>
+			</div>
+		</div>
+	)
+}
+
+export default Header
